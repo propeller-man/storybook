a50d4e4ae1264585fa3a8698d3250286a1e418a3
Constantine <k.vetoshkin@x-run.online>
2024-08-01 17:56:14 +0300

    Create authController.js

---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+exports.register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
