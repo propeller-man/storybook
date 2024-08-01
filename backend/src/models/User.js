0572120f98c9b1e110d2a5d0c1a6a5de8b276687
Constantine <k.vetoshkin@x-run.online>
2024-08-01 17:57:21 +0300

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,1 +1,19 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres'
+});
 
+const User = sequelize.define('User', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+module.exports = User;
