ed0e7587fbc59680cf03db01897ecc428725d2dc
Constantine <k.vetoshkin@x-run.online>
2024-08-01 17:57:54 +0300

    Create index.js

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const config = require('config');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(express.json());
+
+const dbConfig = config.get('db');
+const sequelize = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: 'postgres'
+});
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+
+app.use('/api/auth', authRoutes);
+
+const port = process.env.PORT || 4000;
+app
